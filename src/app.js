b9f265be73a248e9045598be7886e29f06cbf918
Srijan Tripathi <srijantrpth@gmail.com>
2025-01-03 13:29:10 +0530

    Add models and routes for tweets, likes, comments, playlists, and subscriptions

--- src/app.js
+++ src/app.js
@@ -1,38 +1,44 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
-import cors from 'cors';
-import userRouter from './routes/user.routes.js';
-import healthcheckRouter from './routes/healthcheck.routes.js';
-import tweetRouter from './routes/tweet.routes.js';
-import subscriptionRouter from './routes/subscription.routes.js';
-import videoRouter from './routes/video.routes.js';
-import commentRouter from './routes/comment.routes.js';
-import likeRouter from './routes/like.routes.js';
-import playlistRouter from './routes/playlist.routes.js';
-import dashboardRouter from './routes/dashboard.routes.js';
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-})) // used for middleware and user configurations
+    credentials: true,
+  })
+); // used for middleware and user configurations
 
-app.use(express.json({limit: "16kb"})) // limit of JSON which is to be accepted
-app.use(express.urlencoded({extended: true, limit: "16kb"})) // we can continue without giving extended keyword as well. with the use of extended we can give nested objects.
-app.use(express.static("public")) // used to store assets such as images or files such as pdf on server in our case folder name is public
-app.use(cookieParser()) // used to access cookies of user
+app.use(express.json({ limit: "16kb" })); // limit of JSON which is to be accepted
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // we can continue without giving extended keyword as well. with the use of extended we can give nested objects.
+app.use(express.static("public")); // used to store assets such as images or files such as pdf on server in our case folder name is public
+app.use(cookieParser()); // used to access cookies of user
 
 // Routes Import
 
-import userRouter from './routes/user.routes.js'
-
+import userRouter from "./routes/user.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
 // Routes declaration
-app.use("/api/v1/users", userRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
-
-
-export {app} 
+export { app };
